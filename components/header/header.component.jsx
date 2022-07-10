711aa659ffb9a1fe4f40db66f4c763fada4fb279
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-10 23:11:00 +0100

    gradually building pages

---
+++ components/header/header.component.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <h1>weLyft</h1>
+        <p>Taking you places seamlessly and comfortably</p>
+      </div>
+    );
+}
+ 
+export default Header;
---
+++ pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/header.component";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Header/>
+    </div>
+  );
+};
+
+export default Homepage;
