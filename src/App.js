3239779fbf875758b05dd679e26b4a6c6185cd6b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-21 15:19:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+
 import './App.css';
-import Homepage from '../pages/homepage/homepage.component';
+// import Homepage from '../pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+    <p> Hi </p>
     </div>
   );
 }
