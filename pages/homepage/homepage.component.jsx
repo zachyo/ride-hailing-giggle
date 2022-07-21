269426f91d3df5fd5beccc5ad96ec0db0564b785
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-21 15:19:53 +0100

    Update homepage.component.jsx

--- pages/homepage/homepage.component.jsx
+++ pages/homepage/homepage.component.jsx
@@ -1,4 +1,6 @@
-import Header from "../../components/header/header.component";
+import React from "react";
+
+// import Header from "../../components/header/header.component";
 
 const Homepage = () => {
   return (
