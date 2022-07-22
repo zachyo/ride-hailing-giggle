204ed84150dd64f44e9ab757df548b91da44a3e6
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-22 01:58:17 +0100

    homepage

---
+++ Client/src/components/book-a-ride/bookRide.component.jsx
@@ -0,0 +1,8 @@
+const BookRide = () => {
+    return ( <div className="book">
+        <div className="dot"></div>
+        <p>Looking for a ride</p>
+    </div> );
+}
+ 
+export default BookRide;
---
+++ Client/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="info">
+        <p className="welcome">welcome to contexi</p>
+        <h1>
+          Your first choice <br />
+          for traveling anywhere
+        </h1>
+        <div className="left-img"><img src="../assets/contexi.jpg" alt="smiling girl" /></div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
