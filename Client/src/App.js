7adbb3b8edae236cabf8b1722484311d4e2dfe86
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-31 20:47:16 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,11 @@
+import OnBoarding1 from "./components/book-a-ride/bookRide.component";
 import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
         <Homepage/>
+        {/* <OnBoarding1/> */}
     </div>
   );
 }
