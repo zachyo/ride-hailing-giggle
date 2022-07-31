afb578857ddcde6dc6a580315dd7f91d6d82abec
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-31 20:47:12 +0100

    Update bookRide.component.jsx

--- Client/src/components/book-a-ride/bookRide.component.jsx
+++ Client/src/components/book-a-ride/bookRide.component.jsx
@@ -1,8 +1,11 @@
-const BookRide = () => {
-    return ( <div className="book">
-        <div className="dot"></div>
-        <p>Looking for a ride</p>
-    </div> );
-}
- 
-export default BookRide;
+const OnBoarding1 = () => {
+  return (
+    <div className="book">
+      <div className="dot"></div>
+      <h1>Registration</h1>
+      <p>Perfect ride booking web-app for your safety and comfortable trip</p>
+    </div>
+  );
+};
+
+export default OnBoarding1;
