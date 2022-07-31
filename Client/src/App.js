379f17a9595704c717302896dcf306fe3ad5d098
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-31 20:49:15 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,8 @@
 import OnBoarding1 from "./components/onboarding/onboarding.component";
 import Homepage from "./pages/homepage/homepage.component";
 
+
+//mobile first webapp
 function App() {
   return (
     <div className="App">
