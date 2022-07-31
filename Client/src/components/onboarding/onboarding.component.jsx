c7bb9ab94255a56e4ee3f995dd75be984e19186c
zacheusIsrael <zacheazy99@yahoo.com>
2022-07-31 20:48:34 +0100

    Create onboarding.component.jsx

---
+++ Client/src/components/onboarding/onboarding.component.jsx
@@ -0,0 +1,11 @@
+const OnBoarding1 = () => {
+  return (
+    <div className="book">
+      <div className="dot"></div>
+      <h1>Registration</h1>
+      <p>Perfect ride booking web-app for your safety and comfortable trip</p>
+    </div>
+  );
+};
+
+export default OnBoarding1;
