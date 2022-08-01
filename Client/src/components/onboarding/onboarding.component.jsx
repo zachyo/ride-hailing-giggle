603800b6c34c687c2eab2220a23da4421e18c9bf
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-01 22:10:03 +0100

    Update onboarding.component.jsx

--- Client/src/components/onboarding/onboarding.component.jsx
+++ Client/src/components/onboarding/onboarding.component.jsx
@@ -1,11 +1,16 @@
-const OnBoarding1 = () => {
+import { useState } from "react";
+import OnBoarding1 from "./onboarding1.component";
+
+
+const OnBoarding = () => {
+
+  const [page1 , SetPage1] = useState(true)
+
   return (
-    <div className="book">
-      <div className="dot"></div>
-      <h1>Registration</h1>
-      <p>Perfect ride booking web-app for your safety and comfortable trip</p>
+    <div className="booking container h-screen flex flex-col justify-center items-center bg-homeBlue">
+      {page1 && <OnBoarding1/>}
     </div>
   );
 };
 
-export default OnBoarding1;
+export default OnBoarding;
