2cf163d12f6405384bcf44c48027ba12e8d3fdd8
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-02 18:00:35 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import OnBoarding1 from "./components/onboarding/onboarding.component";
 import {Routes, Route} from 'react-router-dom'
 import BookRide from "./components/booking/booking";
+import Rides from "./components/rides/rides";
 // import Homepage from "./pages/homepage/homepage.component";
 
 
@@ -11,7 +12,8 @@ function App() {
       {/* <Homepage/> */}
       <Routes>
         <Route exact path="/" element={<OnBoarding1 />} />
-        <Route path="/book" element={<BookRide/>} />
+        <Route path="/book" element={<BookRide />} />
+        <Route path="/rides" element={<Rides />} />
       </Routes>
     </div>
   );
