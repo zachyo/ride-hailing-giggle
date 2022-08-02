ddbdf20506cb0d24b13a6477ba10fd909444bcbc
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-02 17:14:59 +0100

    Create booking.jsx

---
+++ Client/src/components/booking/booking.jsx
@@ -0,0 +1,12 @@
+const BookRide = () => {
+  return (
+    <div className="">
+      <form action="">
+        <input type="text" name="current_location" placeholder="Where from?" />
+        <input type="text" name="destination" id="" placeholder="Where to?" />
+      </form>
+    </div>
+  );
+};
+
+export default BookRide;
