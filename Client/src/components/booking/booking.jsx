acf61085525c7239423921531aa3abc13c9fc152
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-02 17:43:20 +0100

    Update booking.jsx

--- Client/src/components/booking/booking.jsx
+++ Client/src/components/booking/booking.jsx
@@ -1,9 +1,20 @@
 const BookRide = () => {
   return (
-    <div className="">
-      <form action="">
-        <input type="text" name="current_location" placeholder="Where from?" />
-        <input type="text" name="destination" id="" placeholder="Where to?" />
+    <div className="container">
+      <form action="" className="p-16 flex flex-col space-y-4 w-1/3">
+        <input
+          type="text"
+          name="current_location"
+          placeholder="Where from?"
+          className="border-2 border-black p-2 rounded-lg"
+        />
+        <input
+          type="text"
+          name="destination"
+          id=""
+          placeholder="Where to?"
+          className="border-2 border-black p-2 rounded-lg"
+        />
       </form>
     </div>
   );
