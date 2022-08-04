8a4115cbf4b68de623f90d5695d53d9782c756c3
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-04 19:15:12 +0100

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,19 +3,22 @@ import {Routes, Route} from 'react-router-dom'
 import BookRide from "./components/booking/booking";
 import Rides from "./components/rides/rides";
 import Login from "./pages/login/login";
-// import Homepage from "./pages/homepage/homepage.component";
+import Navbar from "./components/Navbar/navbar.component";
+import Homepage from "./pages/homepage/homepage.component";
 
 
 //mobile first webapp
 function App() {
   return (
     <div className="App">
+      <Navbar />
       {/* <Homepage/> */}
       <Routes>
         <Route exact path="/" element={<OnBoarding1 />} />
         <Route path="/book" element={<BookRide />} />
         <Route path="/rides" element={<Rides />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/ride" element={<Homepage/>} />
       </Routes>
     </div>
   );
