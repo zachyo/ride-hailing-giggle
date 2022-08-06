2fd461f044b2679cd2802bf3cac1db2b529d7aba
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-06 02:43:09 +0100

    Update estimate.jsx

--- Client/src/components/estimate.jsx
+++ Client/src/components/estimate.jsx
@@ -1,13 +1,21 @@
 const Estimate = () => {
     return (
-      <div className="estimate flex space-x-8 mx-auto items-center justify-between w-2/3">
-        <h1>
+      <div className="estimate flex mx-auto items-center justify-between w-2/3 py-10">
+        <h1 className="font-bold text-3xl ">
           Ready to roll? <br /> Get an estimate.
         </h1>
         <div className="flex space-x-8">
-          <input type="text" placeholder="Enter pick-up location*" />
-          <input type="text" placeholder="Enter drop-off location*" />
-          <button className="border border-black">Get estimate</button>
+          <input
+            type="text"
+            placeholder="Enter pick-up location*"
+            className="p-3 border border-black rounded-lg px-8"
+          />
+          <input
+            type="text"
+            placeholder="Enter drop-off location*"
+            className="p-3 border border-black rounded-lg px-8"
+          />
+          <button className="border border-black p-2 px-4 rounded-full font-bold text-white bg-black">Get estimate</button>
         </div>
       </div>
     );
