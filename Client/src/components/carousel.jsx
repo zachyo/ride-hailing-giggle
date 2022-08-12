3e6f48701641ec637eeb363b7a8047aa82f8f823
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-12 17:53:57 +0100

    Update carousel.jsx

--- Client/src/components/carousel.jsx
+++ Client/src/components/carousel.jsx
@@ -1,5 +1,59 @@
 const Reviews = () => {
-    return ( <div className="reviews">Ride Reviews</div> );
-}
- 
+  return (
+    <div className="reviews w-2/3 mx-auto py-8">
+      Ride Reviews
+      <div className="flex p-8 flex-wrap justify-center items-center">
+        <div className="flex flex-col w-1/4 mx-8 items-center">
+          <img src="" alt="1" />
+          <p className="text-center">
+            “As a student, it’s hard to complete my class work around a
+            schedule. I started driving more with Lyft and realized it was the
+            perfect opportunity to make money and work on my own time! Driving
+            with Lyft gives me freedom in my schedule to focus on school, which
+            is my main priority.”
+          </p>
+          <p>- Harold</p>
+          <p>Driving with weRide since 2020</p>
+        </div>
+        <div className="flex flex-col w-1/4 mx-8 items-center">
+          <img src="" alt="2" />
+          <p className="text-center">
+            “As a student, it’s hard to complete my class work around a
+            schedule. I started driving more with Lyft and realized it was the
+            perfect opportunity to make money and work on my own time! Driving
+            with Lyft gives me freedom in my schedule to focus on school, which
+            is my main priority.”
+          </p>
+          <p>- Harold</p>
+          <p>Driving with weRide since 2020</p>
+        </div>
+        <div className="flex flex-col w-1/4 mx-8 items-center">
+          <img src="" alt="3" />
+          <p className="text-center">
+            “As a student, it’s hard to complete my class work around a
+            schedule. I started driving more with Lyft and realized it was the
+            perfect opportunity to make money and work on my own time! Driving
+            with Lyft gives me freedom in my schedule to focus on school, which
+            is my main priority.”
+          </p>
+          <p>- Harold</p>
+          <p>Driving with weRide since 2020</p>
+        </div>
+        {/* <div className="flex flex-col w-1/4">
+          <img src="" alt="" />
+          <p>
+            “As a student, it’s hard to complete my class work around a
+            schedule. I started driving more with Lyft and realized it was the
+            perfect opportunity to make money and work on my own time! Driving
+            with Lyft gives me freedom in my schedule to focus on school, which
+            is my main priority.”
+          </p>
+          <p>- Harold</p>
+          <p>Driving with weRide since 2020</p>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
 export default Reviews;
