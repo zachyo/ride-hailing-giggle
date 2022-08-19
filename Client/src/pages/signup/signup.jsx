96938bd9d813d2adc8044950c8fd122bbe6c1599
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-19 23:34:59 +0100

    Update signup.jsx

--- Client/src/pages/signup/signup.jsx
+++ Client/src/pages/signup/signup.jsx
@@ -68,7 +68,12 @@ const SignUp = () => {
               >
                 Next
               </button>
-              <input type="checkbox" name="Terms of Service" id="terms" required />
+              <input
+                type="checkbox"
+                name="Terms of Service"
+                id="terms"
+                required
+              />
               <label htmlFor="terms">
                 I agree to weRide's{" "}
                 <a href="terms" className="underline">
@@ -77,10 +82,57 @@ const SignUp = () => {
               </label>
             </div>
           )}
-          {step === 2 && <div>This is step 2</div>}
+          {step === 2 && (
+            <div className="my-4">
+              <p className="mb-8">
+                Great! We'll need some information from you to get started.
+              </p>
+              <div className="flex flex-wrap">
+                <FormInput
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  onChange={handleChange}
+                  required
+                  className="rounded-lg w-72 p-3 shadow-lg mt-3 mr-6 bg-white border border-darkBlue"
+                />
+                <FormInput
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  onChange={handleChange}
+                  required
+                  className="rounded-lg w-72 p-3 shadow-lg mt-3 mr-6  bg-white border border-darkBlue"
+                />
+                <FormInput
+                  type="email"
+                  name="email address"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  required
+                  className="rounded-lg w-72 p-3 shadow-lg mt-3  bg-white border border-darkBlue"
+                />
+                <FormInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={handleChange}
+                  required
+                  className="rounded-lg w-72 mr-6 p-3 shadow-lg mt-3 mb-10 bg-white border border-darkBlue"
+                />
+                <FormInput
+                  type="number"
+                  name="referral code"
+                  placeholder="Referal Code (Optional)"
+                  onChange={handleChange}
+                  required
+                  className="rounded-lg w-72 p-3 shadow-lg mt-3 mb-10 bg-white border border-darkBlue"
+                />
+              </div>
+            </div>
+          )}
         </form>
         <p className="mt-8">
-          
           Already applied or already a driver?
           <a href="login" className=" text-darkBlue hover:underline ml-1">
             {" "}
