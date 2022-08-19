cc2a0874f34095ab3e1e492394e5495bae2281b5
zacheusIsrael <zacheazy99@yahoo.com>
2022-08-19 16:14:55 +0100

    Update login.jsx

--- Client/src/pages/login/login.jsx
+++ Client/src/pages/login/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   return (
     <div className="login bg-homeBlue  flex items-center justify-center">
       <div className="text-center py-24 px-12 rounded-2xl">
-        <h1 className="font-bold text-5xl mb-24 text-darkBlue">weRide</h1>
+        <h1 className="font-bold text-5xl mb-24 text-darkBlue  border-darkBlue">weRide</h1>
         <div className="">
           <form action="" className="flex flex-col w-80">
             <FormInput
@@ -34,13 +34,31 @@ const Login = () => {
               onChange={handleChange}
               required
             />
+            <p className="relative">
+              <a
+                href="forgotpassword"
+                className="absolute top-5 right-2 text-darkBlue hover:underline"
+              >
+                {" "}
+                Forget password?
+              </a>
+            </p>
             <Link
               to="/"
               className="hover:bg-homeBlue border-2 hover:border-darkBlue hover:text-black rounded-md p-4 font-bold bg-darkBlue text-white text-center mt-12"
-              onClick={()=>console.log(phone, password)}
+              onClick={() => console.log(phone, password)}
             >
               Login
             </Link>
+            <p className="relative flex justify-between px-4 mt-8"> Don't have an account?
+              <a
+                href="signup"
+                className=" text-darkBlue hover:underline"
+              >
+                {" "}
+                Sign up
+              </a>
+            </p>
           </form>
         </div>
       </div>
