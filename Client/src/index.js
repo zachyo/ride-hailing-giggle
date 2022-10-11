b65c96e37a4fda8f434dcc0a3426014b74ca01f7
zacheusIsrael <zacheazy99@yahoo.com>
2022-10-11 01:56:06 +0100

    Update index.js

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import CurrentUserProvider from './contexts/currentUser.context';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <CurrentUserProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </CurrentUserProvider>
 );
 
